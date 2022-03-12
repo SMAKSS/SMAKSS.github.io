74784cd99c0a48a17ff79a275bb3b32ae8209c50
SMAKSS <SMAKSS@users.noreply.github.com>
2022-03-12 20:07:35 +0000

    deploy: e05657d475ff10b0d27f8b0d7bb12829bb5c36ba

--- _nuxt/static/1647112659/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", (function(a,b){return {routes:[a,"\u002F","\u002Ffa","\u002Ffa\u002Fstuff","\u002Ffa\u002Fstuff\u002Fabout",b,a,b]}}("\u002Fstuff\u002Fabout","\u002Fstuff")))
